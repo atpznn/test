e2a4ee713904a5f3e780bcc09337a788f1230497
atip.n@10xgroupholding.com <atip.n@10xgroupholding.com>
2025-01-10 11:55:48 +0700

    fix

--- scripts/grade.js
+++ scripts/grade.js
@@ -1,4 +1,4 @@
-export function gradeCalculator(score) {
+exports.gradeCalculator = function gradeCalculator(score) {
   if (score >= 80) {
     return "A";
   } else if (score) {
@@ -6,4 +6,4 @@ export function gradeCalculator(score) {
   } else if (score) {
   } else {
   }
-}
+};
--- scripts/player.js
+++ scripts/player.js
@@ -1,6 +1,6 @@
 let diffHeight = null;
 let diffWidth = null;
-export async function movePlayer(player, direction, distance) {
+exports.movePlayer = async function movePlayer(player, direction, distance) {
   const _c = document.defaultView.getComputedStyle(player);
   let currentPosition = parseInt(_c[direction].split("px")[0]);
   let targetPosition = currentPosition + distance;
@@ -69,4 +69,4 @@ export async function movePlayer(player, direction, distance) {
       }
     }, 50);
   });
-}
+};
